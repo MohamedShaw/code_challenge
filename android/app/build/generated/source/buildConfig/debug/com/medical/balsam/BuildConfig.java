67ab13fddc2ef9b906b52293f08959af4ad07ce1
MohamedShawky <shawkymohamed138@gmail.com>
2020-12-28 22:48:19 +0200

    apk

---
+++ android/app/build/generated/source/buildConfig/debug/com/medical/balsam/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.medical.balsam;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.medical.balsam";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10704;
+  public static final String VERSION_NAME = "10704";
+}
---
+++ android/app/build/generated/source/buildConfig/release/com/medical/balsam/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.medical.balsam;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.medical.balsam";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10704;
+  public static final String VERSION_NAME = "10704";
+}
